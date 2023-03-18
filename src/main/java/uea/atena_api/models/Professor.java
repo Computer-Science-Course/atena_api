2c0fd31acf382c32be31d409700c4a6f5dc7c1dc
Mateus Santos <a.mateussm1.5@gmail.com>
2023-03-17 23:49:14 -0300

    Create model Professor

--- src/main/java/uea/atena_api/models/Professor.java
+++ src/main/java/uea/atena_api/models/Professor.java
@@ -1,5 +1,65 @@
 package uea.atena_api.models;
 
-public class Professor {
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Professor implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank(message="Nome é obrigatório")
+	private String nome;
+
+	public Professor() {
+	}
+
+	public Professor(Long codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professor other = (Professor) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
 
 }
