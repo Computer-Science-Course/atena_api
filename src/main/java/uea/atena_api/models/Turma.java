bf06f74e4faf0655f40305568d361c7e0a040bec
Neill-Ataide <newdonataide2k19@gmail.com>
2023-03-18 01:07:10 -0400

    Create model Turma

--- src/main/java/uea/atena_api/models/Turma.java
+++ src/main/java/uea/atena_api/models/Turma.java
@@ -1,5 +1,78 @@
 package uea.atena_api.models;
 
-public class Turma {
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Turma implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank(message="Nome turma obrigatório")
+	private String nome;
+	
+	@Embedded
+	private Professor professor;
+	
+	public Turma() {
+	}
+	
+	public Turma(Long codigo, String nome, Professor professor) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.professor = professor;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Turma other = (Turma) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+	
 }
