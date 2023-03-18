56691ffd7b3662f8e58c34bcd6fb29c56c6a1cac
Neill-Ataide <newdonataide2k19@gmail.com>
2023-03-18 15:14:02 -0400

    Correct architecture
    Changes:
    	* Model Aluno
    	* Model Turma

--- src/main/java/uea/atena_api/models/Aluno.java
+++ src/main/java/uea/atena_api/models/Aluno.java
@@ -1,5 +1,79 @@
 package uea.atena_api.models;
 
-public class Aluno {
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Aluno implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
+	@NotBlank(message = "Nome é obrigátorio")
+	private String nome;
+
+	@NotNull(message = "Matricula obrigátorio")
+	private Long matricula;
+
+	public Aluno() {
+	}
+
+	public Aluno(Long codigo, String nome, Long matricula) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.matricula = matricula;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
 
 }
--- src/main/java/uea/atena_api/models/Turma.java
+++ src/main/java/uea/atena_api/models/Turma.java
@@ -1,5 +1,72 @@
 package uea.atena_api.models;
 
-public class Turma {
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Turma implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank(message = "Nome é obrigátorio")
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name="codigo_professor")
+	private Professor professor;
+
+	public Turma() {
+	}
+
+	public Turma(Long codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Turma other = (Turma) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
 
 }
