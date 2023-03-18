2f7bed0f94cfb4524e64c3379cbc3d17e5a614f6
Neill-Ataide <newdonataide2k19@gmail.com>
2023-03-18 00:47:28 -0400

    Create model Aluno

--- src/main/java/uea/atena_api/models/Aluno.java
+++ src/main/java/uea/atena_api/models/Aluno.java
@@ -1,5 +1,90 @@
 package uea.atena_api.models;
 
-public class Aluno {
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Aluno implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank(message="Nome é obrigatório")
+	private String nome;
+	
+	@NotBlank(message="Numero matrícula obrigatório")
+	private Long matricula;
+	
+	@Embedded
+	private Turma turma;
+	
+	public Aluno() {
+	}
+	
+	public Aluno(Long codigo, String nome, Long matricula, Turma turma) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.matricula = matricula;
+		this.turma = turma;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+	
 }
