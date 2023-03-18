364abc405532050a5a6efad4fd5fbcd29c2f2d77
itvicky22 <italavicky35@gmail.com>
2023-03-18 19:44:10 -0400

    Create model TurmaAluno

---
+++ src/main/java/uea/atena_api/models/TurmaAluno.java
@@ -0,0 +1,78 @@
+package uea.atena_api.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class TurmaAluno{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_turma")
+	private Turma turma;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_aluno")
+	private Aluno aluno;
+
+	public TurmaAluno() {
+
+	}
+
+	public TurmaAluno(Long codigo, Turma turma, Aluno aluno) {
+		super();
+		this.codigo = codigo;
+		this.turma = turma;
+		this.aluno = aluno;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TurmaAluno other = (TurmaAluno) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+	
+	
+}
