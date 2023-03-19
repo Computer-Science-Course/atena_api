02a638adfe6a1d204a42ac605e33d44d6e60ca64
itvicky22 <italavicky35@gmail.com>
2023-03-19 12:19:53 -0400

    Create ServiceAluno criar

--- src/main/java/uea/atena_api/services/AlunoService.java
+++ src/main/java/uea/atena_api/services/AlunoService.java
@@ -1,5 +1,19 @@
 package uea.atena_api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uea.atena_api.models.Aluno;
+import uea.atena_api.repositories.AlunoRepository;
+
+@Service
 public class AlunoService {
 
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	public Aluno criar(Aluno aluno) {
+		return alunoRepository.save(aluno);
+	}
+	
 }
