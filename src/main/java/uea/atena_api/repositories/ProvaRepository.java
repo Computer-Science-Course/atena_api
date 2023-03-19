77e25276365ead78de63c06810f045f610ab9a8a
Mateus Santos <a.mateussm1.5@gmail.com>
2023-03-19 16:33:28 -0300

    Create repository Prova

--- src/main/java/uea/atena_api/repositories/ProvaRepository.java
+++ src/main/java/uea/atena_api/repositories/ProvaRepository.java
@@ -1,5 +1,11 @@
 package uea.atena_api.repositories;
 
-public interface ProvaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import uea.atena_api.models.Prova;
+
+@Repository
+public interface ProvaRepository extends JpaRepository<Prova, Long> {
 
 }
