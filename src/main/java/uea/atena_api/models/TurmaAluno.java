e28f49e8ecadcbe937f20c7f43b681bd9a9b10db
Neill-Ataide <newdonataide2k19@gmail.com>
2023-03-19 17:16:30 -0400

    Add @Entity em TurmaAluno

--- src/main/java/uea/atena_api/models/TurmaAluno.java
+++ src/main/java/uea/atena_api/models/TurmaAluno.java
@@ -2,12 +2,14 @@ package uea.atena_api.models;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class TurmaAluno{
 	
 	@Id
