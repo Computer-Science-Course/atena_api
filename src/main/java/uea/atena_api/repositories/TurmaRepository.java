8fd5d907ba239c69a4813c61b2af7e8a6984f112
SayuryKato <skdp.ecp19@uea.edu.br>
2023-03-19 14:33:38 -0400

    Create TurmaRepository

--- src/main/java/uea/atena_api/repositories/TurmaRepository.java
+++ src/main/java/uea/atena_api/repositories/TurmaRepository.java
@@ -1,5 +1,11 @@
 package uea.atena_api.repositories;
 
-public interface TurmaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import uea.atena_api.models.Turma;
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Long>{
 
 }
