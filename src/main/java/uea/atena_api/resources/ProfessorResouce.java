a35550996efd3498e6ef46b0529d970aa1f44426
SayuryKato <skdp.ecp19@uea.edu.br>
2023-03-19 12:20:17 -0400

    Create Atualizacao de Professor/id

--- src/main/java/uea/atena_api/resources/ProfessorResouce.java
+++ src/main/java/uea/atena_api/resources/ProfessorResouce.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import jakarta.validation.Valid;
 import uea.atena_api.models.Professor;
 import uea.atena_api.services.ProfessorService;
 
@@ -25,7 +27,7 @@ public class ProfessorResouce {
 	private ProfessorService professorService;
 	
 	@PostMapping
-	public ResponseEntity<Professor> criar(@RequestBody Professor professor){
+	public ResponseEntity<Professor> criar(@Valid @RequestBody Professor professor){
 		Professor professorSalva = professorService.criar(professor);		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{codigo}").buildAndExpand(professorSalva.getCodigo()).toUri();
 		return ResponseEntity.created(uri).body(professorSalva);
@@ -47,6 +49,12 @@ public class ProfessorResouce {
 	public ResponseEntity<Void> deletar(@PathVariable Long codigo){
 		professorService.deletar(codigo);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{codigo}")
+	public ResponseEntity<Professor> atualizar(@PathVariable Long codigo,@Valid @RequestBody Professor professor){
+		Professor professorSalva = professorService.atualizar(codigo, professor);
+		return ResponseEntity.ok().body(professorSalva);
 	}
 
 }
--- src/main/java/uea/atena_api/services/ProfessorService.java
+++ src/main/java/uea/atena_api/services/ProfessorService.java
@@ -2,6 +2,7 @@ package uea.atena_api.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,12 @@ public class ProfessorService {
 	
 	public void deletar(Long codigo) {
 		professorRepository.deleteById(codigo);
+	}
+	
+	public Professor atualizar(Long codigo, Professor professor) {
+		Professor professorSalva = professorRepository.findById(codigo).orElseThrow();
+		BeanUtils.copyProperties(professor, professorSalva, "codigo");
+		return professorRepository.save(professorSalva);
 	}
 	
 }
