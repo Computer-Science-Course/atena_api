768e280448fefccdc26c121fdd63c78c72264daf
itvicky22 <italavicky35@gmail.com>
2023-03-19 12:31:19 -0400

    Create ServiceAluno listar

--- src/main/java/uea/atena_api/resources/AlunoResource.java
+++ src/main/java/uea/atena_api/resources/AlunoResource.java
@@ -1,9 +1,11 @@
 package uea.atena_api.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +18,22 @@ import uea.atena_api.services.AlunoService;
 @RestController
 @RequestMapping("/aluno")
 public class AlunoResource {
-	
+
 	@Autowired
 	private AlunoService alunoService;
-	
+
 	@PostMapping
-	public ResponseEntity<Aluno> criar(@RequestBody Aluno aluno){
+	public ResponseEntity<Aluno> criar(@RequestBody Aluno aluno) {
 		Aluno alunoSalva = alunoService.criar(aluno);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{codigo}").buildAndExpand(alunoSalva.getCodigo()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{codigo}")
+				.buildAndExpand(alunoSalva.getCodigo()).toUri();
 		return ResponseEntity.created(uri).body(alunoSalva);
 	}
-	
+
+	@GetMapping
+	public ResponseEntity<List<Aluno>> listar() {
+		List<Aluno> aluno = alunoService.listar();
+		return ResponseEntity.ok().body(aluno);
+	}
+
 }
