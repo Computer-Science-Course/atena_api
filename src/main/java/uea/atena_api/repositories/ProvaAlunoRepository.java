554fe40b2e32abb6384d7c85503bb40a35491605
Mateus Santos <a.mateussm1.5@gmail.com>
2023-03-19 16:44:33 -0300

    Create repository ProvaAluno

--- src/main/java/uea/atena_api/repositories/ProvaAlunoRepository.java
+++ src/main/java/uea/atena_api/repositories/ProvaAlunoRepository.java
@@ -1,5 +1,11 @@
 package uea.atena_api.repositories;
 
-public interface ProvaAlunoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import uea.atena_api.models.ProvaAluno;
+
+@Repository
+public interface ProvaAlunoRepository extends JpaRepository<ProvaAluno, Long> {
 
 }
