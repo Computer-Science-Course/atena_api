ebc01ed94918810769c4bc473a75da11ac5142dd
DâmarisCampos <camposdamaris08@gmail.com>
2023-03-20 00:00:56 -0300

    Create ProvaListar

--- src/main/java/uea/atena_api/services/ProvaService.java
+++ src/main/java/uea/atena_api/services/ProvaService.java
@@ -1,5 +1,20 @@
 package uea.atena_api.services;
 
-public class ProvaService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uea.atena_api.models.Prova;
+import uea.atena_api.repositories.ProvaRepository;
+
+@Service
+public class ProvaService {
+	
+	@Autowired
+	private ProvaRepository provaRepository;
+
+	public List<Prova> listar() {
+		return provaRepository.findAll();
+	}
 }
