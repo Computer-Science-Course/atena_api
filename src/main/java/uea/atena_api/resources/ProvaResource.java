6a016a49cf33dc557397e7666a3237f571c74c31
DâmarisCampos <camposdamaris08@gmail.com>
2023-03-20 00:12:33 -0300

    create GetProvas

--- src/main/java/uea/atena_api/resources/ProvaResource.java
+++ src/main/java/uea/atena_api/resources/ProvaResource.java
@@ -1,5 +1,26 @@
 package uea.atena_api.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uea.atena_api.models.Prova;
+import uea.atena_api.services.ProvaService;
+
+@RestController
+@RequestMapping("/provas")
 public class ProvaResource {
 
+	@Autowired
+	private ProvaService provaService;
+	
+	@GetMapping
+	public ResponseEntity<List<Prova>> listar(){
+		List<Prova> prova = provaService.listar();
+		return ResponseEntity.ok().body(prova);
+}
 }
