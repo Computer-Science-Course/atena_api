323c37e52f98fd686dc4b357860b29f994c193e9
Neill-Ataide <newdonataide2k19@gmail.com>
2023-03-24 11:05:30 -0400

    Fix import in AlunoResource

--- src/main/java/uea/atena_api/resources/AlunoResource.java
+++ src/main/java/uea/atena_api/resources/AlunoResource.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import uea.atena_api.models.Aluno;
 import uea.atena_api.services.AlunoService;
